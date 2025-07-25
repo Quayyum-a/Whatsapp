aeee37c99673c38fb302203054c2d1a6430ef2eb
Quayyum <quayyumariyo@gmail.com>
2025-07-25 06:27:47 +0100

    Update app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -12,8 +11,7 @@ export {
   ErrorBoundary,
 } from 'expo-router';
 
-
-
+  
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -42,7 +40,6 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
       <Stack>
