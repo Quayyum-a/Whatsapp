0c53164c7619dfc2a6b2b2f44deb368982dc78da
Quayyum <quayyumariyo@gmail.com>
2025-07-25 17:15:32 +0100

    updated files

--- app/verify/[phone].tsx
+++ app/verify/[phone].tsx
@@ -1,6 +1,6 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useEffect, useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { Stack, useLocalSearchParams } from 'expo-router'
 import {
   CodeField,
   Cursor,
@@ -9,15 +9,22 @@ import {
 } from 'react-native-confirmation-code-field';
 import Colors from '@/constants/Colors';
 
-
+const CELL_COUNT = 6;
 const Page = () => {
   const {phone, signin} = useLocalSearchParams<{phone: string, signin: string}>();
   const [code, setCode] = useState('');
+   const ref = useBlurOnFulfill({ value: code, cellCount: CELL_COUNT });
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value: code,
+    setValue: setCode,
+  });
   useEffect(() => {
     if (code.length === 6) {
-      // Handle code submission
-      console.log(`Code entered: ${code}`);
-      //TODO VERIFY CODE  
+      if(signin === 'true') {
+        verifySignIn();
+      }else {
+        verifyCode();
+      }
     }
   }, [code]);
   const verifyCode = async () => {
@@ -30,6 +37,7 @@ const Page = () => {
 
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ headerTitle: phone }} />
       <Text style={styles.legal}>
         We have sent you an SMS with a code to the number above.
       </Text>
@@ -37,6 +45,35 @@ const Page = () => {
         To complete your phone number verification, please enter the 6-digit
         activation code.
       </Text>
+
+      <CodeField
+        ref={ref}
+        {...props}
+        value={code}
+        onChangeText={setCode}
+        cellCount={CELL_COUNT}
+        rootStyle={styles.codeFieldRoot}
+        keyboardType="number-pad"
+        textContentType="oneTimeCode"
+        renderCell={({ index, symbol, isFocused }) => (
+          <View
+            // Make sure that you pass onLayout={getCellOnLayoutHandler(index)} prop to root component of "Cell"
+            onLayout={getCellOnLayoutHandler(index)}
+            key={index}
+            style={[styles.cellRoot, isFocused && styles.focusCell]}
+          >
+            <Text style={styles.cellText}>
+              {symbol || (isFocused ? <Cursor /> : null)}
+            </Text>
+          </View>
+        )}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={resendCode}>
+        <Text style={styles.buttonText}>
+          Didn't receive a verification code?
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -48,11 +85,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background,
     gap: 20,
   },
-  loading : {
-      backgroundColor: Colors.primary,
-      alignItems: "center",
-      justifyContent: "center",
-  },
+  
   legal: {
     fontSize: 14,
     textAlign: "center",
@@ -65,6 +98,31 @@ const styles = StyleSheet.create({
   buttonText: {
     color: Colors.primary,
     fontSize: 18,
+  },
+  codeFieldRoot: {
+    marginTop: 20,
+    width: 260,
+    marginLeft: "auto",
+    marginRight: "auto",
+    gap: 8,
+  },
+  cellRoot: {
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  cellText: {
+    color: "#000",
+    fontSize: 36,
+    textAlign: "center",
+  },
+  focusCell: {
+    paddingBottom: 4,
+    borderBottomColor: "#000",
+    borderBottomWidth: 2,
   },
 });
 export default Page
