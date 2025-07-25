5d540af33bdbd02a04e11b7801a9f72eb343fb74
Quayyum <quayyumariyo@gmail.com>
2025-07-25 06:27:49 +0100

    Update app/index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,58 @@
-import {View, Text, StyleSheet, Image} from 'react-native';
-import welcomeImage from '@assets/images/welcome.png';
-const welcome_image = Image.resolveAssetSource(welcomeImage).uri;
+import Colors from "@/constants/Colors";
+import { View, Text, StyleSheet, Image } from "react-native";
+const welcome_image = require("../assets/images/welcome.png");
 
 const Page = () => {
+  const openLink = () => {}
   return (
     <View style={styles.container}>
-      <Image source={{uri: welcome_image}} style={styles.welcome} />
-    <Text> FIRST PAGE </Text> 
+      <Image source={welcome_image} style={styles.welcome} />
+      <Text style={styles.headline}> Welcome to WhatsApp </Text>
+      <Text style={styles.description}>
+        Read our{" "}
+        <Text style={styles.link} onPress={openLink}>
+          Privacy Policy
+        </Text>
+        . {'Tap "Agree & Continue" to accept the '}
+        <Text style={styles.link} onPress={openLink}>
+          Terms of Service
+        </Text>
+        .
+      </Text>
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
   welcome: {
-    width: "100%"
-  }
-})
+    width: "100%",
+    height: 300,
+    marginBottom: 80,
+  },
+  headline: {
+    fontSize : 24,
+    fontWeight: 'bold',
+    marginVertical : 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 80,
+    color: Colors.gray
+  },
+  link: {
+    color: Colors.primary,
+  },
+  button: {
+    width: '100%',
+    alignItems: 'center'
+  },
+});
 
-export default Page
+export default Page;
